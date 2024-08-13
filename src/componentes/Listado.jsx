2e92c74a4d9c44f34d8501fbc3f67c61f59627a6
mariadelmarmazu <mariadelmarmazuelasmong@gmail.com>
2024-08-13 12:17:37 +0200

    No carga Listado

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -6,29 +6,38 @@ import { useState, useEffect } from 'react'
 const Listado = () =>{
 
     const [articulos, setArticulos] = useState([])
+
     useEffect(()=>{
         const fetchArticulos = async ()=>{
             try{
-            const response = await fetch(process.env.REACT_URL + '/tienda')
+            const response = await fetch(`${process.env.REACT_APP_URL}/tienda`)
+                if(!response.ok){
+                    throw new Error('¡Error al mostrar los productos!')
+                }
             const data = await response.json()
             setArticulos(data)
             }catch(error){
-                console.log(error.message)
+                console.log(error)
+                //Mostrar un mensaje de error
+                console.log('¡Error al obtener los productos!')
             }
         }
         fetchArticulos()
+        
     }, [])
     
     return(
-        <div>
+        <div className='container m-3'>
             <h1>Listado de Productos</h1>
             {articulos.map(articulo => (
-                <div key={articulo.id_articulo}>
-                    <img src={articulo.imagen} alt={articulo.nombre} />
-                    <h2>{articulo.nombre}</h2>
-                    <p>{articulo.marca}</p>
-                    <p>{articulo.precio}</p>
-                    <Link to={`/tienda/${articulo.id_articulo}`}>Ver Articulo</Link>
+                <div key={articulo.id_articulo} className='card'>
+                    <img src={articulo.imagen} alt={articulo.nombre} className='card-img-top' />
+                    <div className='card-body'>
+                        <h2 className='card-title'>{articulo.nombre}</h2>
+                        <p className='card-text'>{articulo.marca}</p>
+                        <p className='card-text'>{articulo.precio}</p>
+                        <Link to={`/tienda/${articulo.id_articulo}`} className='btn btn-primary'>Ver Articulo</Link>
+                    </div>
                 </div>
             ))}
         </div>
